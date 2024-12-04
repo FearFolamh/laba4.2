0d2eb21b2148b291033268b2d0db69bfa0a0c1e4
I-Unknown-I <62710051+I-Unknown-I@users.noreply.github.com>
2024-12-04 22:05:52 +0300

    0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+
+
+class ComplexNumber
+{
+    double real, imaginary;
+    public  ComplexNumber(double real, double imaginary)
+    {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+    public ComplexNumber add(ComplexNumber other)
+    {
+        double newReal = this.real + other.real;
+        double newImaginary = this.imaginary + other.imaginary;
+        return new ComplexNumber(newReal,newImaginary);
+    }
+    @Override
+    public String toString()
+    {
+        return real+" + "+imaginary+" i";
+    }
+}
+class Main
+{
+    public static void main(String[] args)
+    {
+        ComplexNumber ComplexNumber1 = new ComplexNumber(3,7);
+        ComplexNumber ComplexNumber2 = new ComplexNumber(4,3);
+        ComplexNumber sum =ComplexNumber1.add(ComplexNumber2);
+        System.out.println(ComplexNumber1);
+        System.out.println(ComplexNumber2);
+        System.out.println(sum);
     }
 }
